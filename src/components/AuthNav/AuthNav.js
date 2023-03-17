0a0a9884ffc48b1f4473cb15c2fc05cf727aaf0a
Mykola Pankov <pankoff97@gmail.com>
2023-03-17 15:35:08 +0200

    21/2 std

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,15 +1,24 @@
 import { NavLink } from 'react-router-dom';
 import css from './AuthNav.module.css';
+import styled from 'styled-components';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: red;
+  }
+`;
 
 export const AuthNav = () => {
   return (
     <div>
-      <NavLink className={css.link} to="/register">
+      <StyledLink className={css.link} to="/register">
         Register
-      </NavLink>
-      <NavLink className={css.link} to="/login">
+      </StyledLink>
+      <StyledLink className={css.link} to="/login">
         Log In
-      </NavLink>
+      </StyledLink>
     </div>
   );
 };
