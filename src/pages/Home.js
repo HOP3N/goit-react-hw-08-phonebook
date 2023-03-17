dd5aa9bfa2c52dd088c11f11012dd3e9a96932a5
Mykola Pankov <pankoff97@gmail.com>
2023-03-17 20:20:17 +0200

    21/3 stls

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,12 @@ const styles = {
     justifyContent: 'center',
   },
   title: {
-    fontWeight: 500,
+    fontWeight: 400,
     fontSize: 48,
     textAlign: 'center',
+  },
+  icon: {
+    marginRight: 20,
   },
 };
 
@@ -16,10 +19,10 @@ export default function Home() {
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>
-        Phonebook welcome page{' '}
-        <span role="img" aria-label="Telephone icon">
+        <span style={styles.icon} role="img" aria-label="Telephone icon">
           ☎️
         </span>
+        Phonebook welcomes you!{' '}
       </h1>
     </div>
   );
